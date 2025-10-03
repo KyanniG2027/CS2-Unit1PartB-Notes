19833f554be43721673905bb036ca3371f980543
Kyanni <kyanni.gonzalez2027@gbwl.org>
2025-10-03 12:43:00 +0000

    1.8 Notes

--- Main.java
+++ Main.java
@@ -1,4 +1,15 @@
+/**
+ * @author Kyanni G
+ * @since 3 Oct 2025
+ * @version 1.0
+ * Notes for topics 1.8-1
+ */
 public class Main {
+   /**
+    * main METHOD is the entry point of the program
+    *@param INPUT is an array of Strings
+    @return void means NO OUTPUT 
+    */
 
    public static void main(String []args) {
       System.out.println("Hello World");
